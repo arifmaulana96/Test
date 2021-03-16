ceec138d977a43e22ed6ef832ecb119d6461ff7b
Arif Maulana <79895661+arifmaulana96@users.noreply.github.com>
2021-03-17 00:03:59 +0700

    Update per3-latihan2.php

--- per3-latihan2.php
+++ per3-latihan2.php
@@ -1,29 +1,31 @@
 <html>
 <head>
-<title>contoh Penggunaan IF</title>
+	<title>contoh Penggunaan IF</title>
 </head>
 <body>
-<form>
- Besar Pembelian :
-<input type=text name=total_beli><br><br>
-<input type=submit value="Tentukan Diskon">
-</form>
+	<form method="post">
+	 Besar Pembelian :
+	<input type=text name=bayar><br><br>
+	<input type=submit name="hitung" value="Tentukan Diskon">
+	</form>
 <?php
- if (isset($total_beli))
- {
- $total_beli=intval($total_beli);
- $diskon=0; 
- if($total_beli>=200000)
- $diskon=0.1;
- else if ($total_beli>=100000)
- $diskon= 0.05;
- else
- $diskon=0.01;
+	 if (isset($_POST['hitung'])){
+	 	$bayar   =$_POST['bayar'];
 
- $diskon=$diskon * intval($total_beli);
- printf("Diskon = %s <br>\n", $diskon);
- printf("Pembayaran = %s <br>\n", $total_beli-$diskon);
- }
+		$total_beli=intval($bayar);
+		$diskon=0;
+
+	  	if($total_beli>=200000)
+	 		$diskon=0.1;
+	 	else if ($total_beli>=100000)
+	 		$diskon= 0.05;
+	 	else
+	 		$diskon=0.01;
+
+	 $diskon=$diskon * intval($total_beli);
+	 printf("Diskon = %s <br>\n", $diskon);
+	 printf("Pembayaran = %s <br>\n", $total_beli-$diskon);
+	 }
 ?>
 </body>
-</html>
+</html> 
