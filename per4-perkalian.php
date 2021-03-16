3175365b397c711321f1adca71786b5e876255c1
Arif Maulana <79895661+arifmaulana96@users.noreply.github.com>
2021-03-17 00:38:41 +0700

    Update per4-perkalian.php

--- per4-perkalian.php
+++ per4-perkalian.php
@@ -1,53 +1,20 @@
 <?php
-@$bil_awal = $_POST['bil_awal'];
-@$bil_akhir = $_POST['bil_akhir'];
-@$pengali = $_POST['pengali'];
-@$hasil_perkalian = 0;
-@$hasil_penjumlahan = 0;
-?>
+echo "Tabel Perkalian <br>";
+echo "===========";
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>AKUMULASI PERKALIAN</title>
-    </head>
-    <body>
-        <form method="POST">
-            <table>
-                <tr>
-                    <td>Input Bilangan Awal</td>
-                    <td>=</td>
-                    <td><input name="bil_awal" type="text" value="<?php echo $bil_awal; ?>"/></td>
-                </tr>
-                <tr>
-                    <td>Input Bilangan Akhir</td>
-                    <td>=</td>
-                    <td><input name="bil_akhir" type="text" value="<?php echo $bil_akhir; ?>"/></td>
-                </tr>
-                <tr>
-                    <td>Input Bilangan Pengali (yang akan dikalikan)</td>
-                    <td>=</td>
-                    <td><input name="pengali" type="text" value="<?php echo $pengali; ?>"/></td>
-                </tr>
-                <tr>
-                    <td><input name="submit" type="submit" value="SUBMIT"/></td>
-                </tr>
-                <tr>
-                    <td>
-                        <?php
-                        while ($bil_awal <= $bil_akhir) {
-                            echo $bil_awal . " x " . $pengali." = ";
-                            echo $hasil_perkalian = $bil_awal * $pengali."<br/>";
-                            $bil_awal++;
-                            $hasil_penjumlahan = $hasil_penjumlahan + $hasil_perkalian;
-                        }   
-                        
-                            echo "========+<br/>".$hasil_penjumlahan;
-                        
-                        ?>
-                    </td>
-                </tr>
-            </table>
-        </form>
-    </body>
-</html>
+$arr=array(15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45); 
+
+for($a=12;$a<=12;$a++)
+
+{
+	foreach ($arr as $b) {
+		$jum=$a*$b;
+		echo"<table>";
+
+		echo"<tr><td>$a x $b</td><td>= $jum<br></td></tr>";
+	}
+}
+
+echo"</table>";
+
+?>
