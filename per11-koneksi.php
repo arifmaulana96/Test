cb7afba76b36e6be25e5efb7b78aa076dd86f45c
Arif Maulana <79895661+arifmaulana96@users.noreply.github.com>
2021-03-25 18:05:07 +0700

    koneksi kedatabase

---
+++ per11-koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+// buat koneksi dengan database mysql
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "buku_tamu";
+$koneksi = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+?>
